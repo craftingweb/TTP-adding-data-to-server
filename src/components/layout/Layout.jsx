b011041a5adaace15c04b687902dc076aaa53fe0
Vitali <vitaliy.prymak@cix.csi.cuny.edu>
2023-07-08 14:18:57 -0400

    removing git rm -r

--- src/components/layout/Layout.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { Outlet } from "react-router-dom";
-
-export const Layout = ({ children }) => {
-  return <div className="bg-gray-900 bg-auto py-10">{children}</div>;
-};
